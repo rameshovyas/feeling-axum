51e73c3d57a3f12032ca8450df491e7d45109867
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-06 18:41:59 +0530

    Added a route that will handle the post request and will echo back the string posted

--- post_string/src/routes/mod.rs
+++ post_string/src/routes/mod.rs
@@ -1,14 +1,17 @@
 mod index;
 mod users;
-use axum::{Router, body::Body, routing::get};
+mod echo_post_string;
+use axum::{Router, body::Body, routing::get,routing::post};
 use index::index;
 use users::users;
+use echo_post_string::echo_post_string;
 
 // public function that returns handle to all routers
 pub fn create_routes() -> Router<(),Body> {
     Router::new()
             .route("/", get(index))
             .route("/users",get(users))
+            .route("/echo_post_string", post(echo_post_string))
 }
 
 

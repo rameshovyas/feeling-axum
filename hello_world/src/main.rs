f9cfe95ae3337b6680fe1327d18e338f2fd10b20
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-06 12:21:55 +0530

    Explicit handler function added to handle to /

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,11 +4,16 @@ use axum::{Router, routing::get};
 async fn main() {
     
     //Create a route for home page
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/",get(index));
     
     //Using Hyper to run this server
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn index() -> String {
+    "Hello World!".to_owned()
 }

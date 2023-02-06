47252dedd680b5068d383e3bd21667d220e01022
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-06 12:14:12 +0530

    Spinned a simple server with the help of hyper

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,14 @@
+use axum::{Router, routing::get};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    
+    //Create a route for home page
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    
+    //Using Hyper to run this server
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }

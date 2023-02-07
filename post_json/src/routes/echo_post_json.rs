2db91a8fe105527f8b1e769323fffa0ff5f82048
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-07 08:04:44 +0530

    Added a route handler for extracting posted json and echo back it to the client

--- post_json/src/routes/echo_post_json.rs
+++ post_json/src/routes/echo_post_json.rs
@@ -1,3 +1,17 @@
-pub async fn echo_post_json() {
-    
+use axum::{Json};
+use serde::{Deserialize,Serialize};
+
+#[derive(Serialize,Deserialize, Debug)]
+pub struct PostedJsonData {
+    data : String,
+}
+
+#[derive(Serialize)]
+pub struct JsonResponseFromServer {
+    data : String,
+    message_from_server: String
+}
+
+pub async fn echo_post_json( Json(body): Json<PostedJsonData>) -> Json<JsonResponseFromServer> {
+       Json(JsonResponseFromServer { data: body.data, message_from_server: "Thank you for posting JSON data".to_owned()})
 }

e7c63f0e1240f508fd5ac2ad7c232917d64cf9c9
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-08 19:45:37 +0530

    Project demonstrating query parameters added

---
+++ query_params/src/routes/mod.rs
@@ -0,0 +1,28 @@
+mod index;
+mod users;
+mod echo_post_string;
+mod echo_post_json;
+mod path_variables;
+mod query_params;
+
+use axum::{Router, body::Body, routing::get,routing::post};
+use index::index;
+use users::users;
+use echo_post_string::echo_post_string;
+use echo_post_json::echo_post_json;
+use path_variables::path_variables;
+use query_params::query_params;
+// public function that returns handle to all routers
+pub fn create_routes() -> Router<(),Body> {
+    Router::new()
+            .route("/", get(index))
+            .route("/users",get(users))
+            .route("/echo_post_string", post(echo_post_string))
+            .route("/echo_post_json", post(echo_post_json))
+            .route("/path_variables/:id", get(path_variables))
+            .route("/query_params", get(query_params))
+}
+
+
+
+
